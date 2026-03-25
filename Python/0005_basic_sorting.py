872308a04a5f7e5456a231845a596af7d5f18f6e
Ashutosh Mishra <mishraashutosh801@gmail.com>
2026-03-25 23:40:21 +0530

    Added Bubble, selection and Insertion sorting implementation.

---
+++ Python/0005_basic_sorting.py
@@ -0,0 +1,70 @@
+# Implementation of Selection sort:
+'''
+1. Assume the array has two parts one is sorted and second is unsorted.
+2. Initially sorted part will have zero values, all values are in unsorted part.
+3. From the last look for the smallest, swap it with 0th index element. size of sorted part is now 1.
+4. now repeat this for the remaining unsorted array, swap it with index 1.
+5. continue this till only one element remains in the unsorted part, this is largest element.
+Complexity: n+(n-1)+(n-2)+.......+0 = n(n+1)/2 = O(n^2)
+
+'''
+def selectionSort(arr):
+    return do_sort(arr, 0)
+
+def do_sort(arr, i):
+    if i==len(arr)-1:
+        return arr
+    min = i
+    for j in range(i, len(arr)):
+        if arr[j] < arr[min]:
+            min = j
+    arr[i], arr[min] = arr[min], arr[i]
+    return do_sort(arr, i+1)
+
+# Implementation of Bubble sort:
+'''
+1. Assume the array as bubbles in water, Bigger bubble will go to the surface first.
+2. Iterate over the array and compare each adjecent pair, swap if arr[j] > arr[j+1],
+after an iteration, the largest number (biggest bubble) will be places at the end of the array.
+3. Since in each iteration one number will be placed in the end of the array, we don't
+need iterate over all the numbers in the array on next iteration. Hence the outer loop
+should run i=n-1 to 0 (n times) but inner loop should run j=0 to i-1.
+Complexity: O(n*(n+1)/2) = O(n^2) like selection sort.
+For best effort case (array already sorted) we can add a flag in the outer loop is_swapped=false,
+and set it to true if any swap happens in ineer loop. for already sorted array this will 
+remain false after the inner loop execution, break the outer loop and return in this case. The 
+complexity will be O(n) here.
+'''
+def bubbleSort(arr):
+    for i in range(len(arr)-1, -1, -1): # running loop from n-1 to 0
+        for j in range(i):
+            if arr[j]>arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+# Implementation of Insertion sort:
+'''
+1. select the part of array from left of size 1, insert next element in this array at 
+correct place by left shiftiing all large numbers.
+2. Now after this insertion, we have array of size 2 sorted.
+3. repeat this process with the next element by inserting it at its right place in the 
+current array of size 2, after this we'll have sorted array of size 3.
+4. Repeat this process for all elements.
+Complexity: For worst and avg case complexity will be O(n^2) for best case it will 
+be O(n) because no right shift needed if array is already sorted.
+'''
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        for j in range(i, 0, -1):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+    return arr
+            
+
+if __name__ == "__main__":
+    print("Enter array to sort: ")
+    input_arr = list(map(int, input().split()))
+    print(f"Selection Sorted array is {selectionSort(input_arr.copy())}")
+    print(f"Bubble Sorted array is {bubbleSort(input_arr.copy())}")
+    print(f"Insertion Sorted array is {insertionSort(input_arr.copy())}")
+
