e74fa2b840c8e54e9660bd1e8b68916f1bed5309
Ashutosh Mishra <mishraashutosh801@gmail.com>
2026-03-29 00:06:29 +0530

    Adding solution of Problem 'Find Kth largest element'.

---
+++ Python/002_array_Kth_largest.py
@@ -0,0 +1,34 @@
+
+# Solution: In quick sort, check if the partition is K and return value if true.
+def findKthLargest(arr, k):
+    return quickSelect(arr, k, 0, len(arr)-1)
+
+def quickSelect(arr, k, start, end):
+    if start<=end:
+        p = partition(arr, start, end)
+        if p==k:
+            return arr[k]
+        res = quickSelect(arr, k, start, p-1)
+        if res == -1:
+            res = quickSelect(arr, k, p+1, end)
+        return res
+    return -1
+
+def partition(arr, start, end):
+    pivot = int((start+end)/2)
+    i, j = start, end
+    while i<=j:
+        while (i<=end and arr[i]<=arr[pivot]):
+            i+=1
+        while (j>=start and arr[j]>arr[pivot]):
+            j-=1
+        if i<j:
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[j], arr[pivot] = arr[pivot], arr[j]
+    return j
+
+if __name__=="__main__":
+    print("Enter Array input: ")
+    arr = list(map(int, input().split()))
+    k = int(input("Enter value of k: "))
+    print(f"Kth largest number in arrar is {findKthLargest(arr, k)}")
